376bb5d0cfdb4d0df8c0b55d85a5cf7f8b422cc8
Dmitry Permyakov <taren4ik@mail.ru>
2022-03-17 00:00:01 +1000

    поставил django toolbar

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
+
 handler403 = "core.views.csrf_failure"
 handler404 = "core.views.page_not_found"
 handler500 = "core.views.server_error"
@@ -15,6 +16,8 @@ urlpatterns = [
     path("about/", include("about.urls", namespace="about")),
 ]
 if settings.DEBUG:
+    import debug_toolbar
     urlpatterns += static(
         settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
     )
+    urlpatterns += (path('__debug__/', include(debug_toolbar.urls)),)
